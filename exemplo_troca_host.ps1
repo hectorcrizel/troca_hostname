e1047d0a63979067f4f51c22700e35375639042f
hectorcrizel <146693049+hectorcrizel@users.noreply.github.com>
2024-07-25 16:21:04 -0300

    Exemplo Script

---
+++ exemplo_troca_host.ps1
@@ -0,0 +1,65 @@
+﻿
+      _/    _/     _/_/      _/_/    _/   _/   _/_/_/    _/   _/    _/_/
+   _/  _/_/  _/  _/   _/  _/    _/  _/  _/    _/    _/  _/   _/  _/     
+  _/   _/   _/  _/   _/  _/    _/  _/_/      _/_/_/    _/   _/    _/_/  
+ _/        _/  _/   _/  _/    _/  _/  _/    _/  _/    _/   _/        _/ 
+_/        _/    _/_/   _/    _/  _/    _/  _/    _/    _/_/     _/_/    
+
+
+              Adobe Photoshop 2024 (v25.0) Multilingual
+                                (C)
+                           www.adobe.com
+
+
+       SUPPLiER.......: m0nkrus     RELEASE DATE....: 09/14/23
+       CREATiON.......: m0nkrus     RELEASE TYPE....: Full Distr
+       TESTER.........: m0nkrus     PROTECTiON......: Cracked
+
+
+                    ... RELEASE iNFORMATiON ...
+
+
+          Adobe Photoshop 2024 (v25.0) Multilingual contains
+       Chinese std, Chinese trad, Czech, Danish, Dutch, English
+    (International), English (North America), English with Arabic
+    support, English with Hebrew support, Finnish, French, French
+  (Canada), French (Morocco), German, Hungarian, Italian, Japanese,
+  Korean, Norwegian, Polish, Portuguese (Brazil), Russian, Spanish,
+      Spanish (Mexico), Swedish, Turkish and Ukrainian versions
+              of Adobe Photoshop 2024 for 64bit(x64).
+
+
+                      ... iNSTALL iNFO ...
+
+
+  1.) Install the Adobe Photoshop 2024 (use autoplay.exe).
+
+  2.) Enjoy!
+
+
+  Note: If you encounter any issues with a previous installation / crack,
+  please uninstall Adobe Photoshop 2024 and delete those folders:
+
+  C:\Program Files (x86)\Common Files\Adobe\SLCache
+  C:\ProgramData\Adobe\SLStore
+
+
+                      ... FOR RUSSiANS ...
+
+
+     Хочу заявить, что считаю администрацию ресурса NNM-Club 
+                ПОДЛОЙ
+                         ЛЖИВОЙ
+                                  И БЕСПРИНЦИПНОЙ!
+     Публикацию новых релизов и всяческое общение с юзерами
+      на NNM-Club я прекращаю. Перенос моих сборок на этот
+                 ресурс прошу не производить!
+  О причинах моей нелюбви к данному ресурсу можете прочитать на 
+              http://www.monkrus.ws/p/nnm-club.html
+
+
+                      ... CONTACT iNFO ...
+
+
+                      Site: WWW.MONKRUS.WS
+                    E-mail: MONKRUS@GMAIL.COM
